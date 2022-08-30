7a53b1d2e9b1324f3df38517ff4cf6592088c6e7
Hana Widyatari <hanawidyatari23@gmail.com>
2022-08-30 17:09:11 +0700

    get data from API

--- components/cards/ProductCard.js
+++ components/cards/ProductCard.js
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
+
 import EditButton from "../buttons/EditButton";
 import DeleteButton from "../buttons/DeleteButton";
 
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,16 +1,34 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import { urlWebApi } from "../../utility/urlApi";
+import { hasToken } from "../../utility/localStorage";
+
 import PageHeader from "../../components/headers/PageHeader";
 import ProductCard from "../../components/cards/ProductCard";
 
-import { product } from "../../utility/Products";
-
 export default function index() {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    hasToken();
+    axios
+      .get(`${urlWebApi}v1/products`)
+      .then((response) => {
+        setProductList(response.data.result);
+        // dispatch(getCustomers(response.data.result));
+      })
+      .catch((error) => error);
+  }, []);
+
   return (
     <div>
       <div className="border border-b-gray-800 py-8">
         <PageHeader title="Product List" titleButton="Create New" />
       </div>
-      <div className="mt-10 grid grid-cols-3 w-[1300px] h-[950px] mx-10 ml-[150px]">
-        {product.map((item, i) => {
+      <div className="mt-10 grid grid-cols-3 w-[1300px] h-[950px] mx-10 ml-[150px] gap-y-10">
+        {productList.map((item, i) => {
           return (
             <div key={i}>
               <ProductCard name={item.name} price={item.price} id={item.id} image={item.imageurl} />
