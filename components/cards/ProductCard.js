a661eb8ec0893f438f0d9d5f2970ffe3f047b3ea
Hana Widyatari <hanawidyatari23@gmail.com>
2022-08-30 16:07:59 +0700

    update background product

--- components/cards/ProductCard.js
+++ components/cards/ProductCard.js
@@ -1,16 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import EditButton from "../buttons/EditButton";
 import DeleteButton from "../buttons/DeleteButton";
 
 export default function ProductCard(props) {
-  // const image = props.image;
   return (
     <div>
-      <div className="h-fit w-fit border border-gray-800 bg-slate-800">
+      <div className="h-fit w-fit border border-gray-900" style={{ backgroundImage: `url(${props.image})`, backgroundRepeat: `no-repeat`, backgroundSize: `cover` }}>
         <div className="h-[300px] w-[350px] flex justify-end gap-5 pt-4 pr-5">
           <EditButton name={props.name} price={props.price} id={props.id} image={props.image} />
           <DeleteButton name={props.name} id={props.id} />
         </div>
-        <div className="bg-white h-[100px]">
+        <div className="bg-white h-[100px] w-full border border-t-gray-900">
           <div className="mx-6 py-6">
             <p className="text-xl mb-2">{props.name}</p>
             <p className="text-sm">$ {props.price}</p>
