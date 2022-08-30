a681baf272bc3c4e5217bba4dcde8c45b307e549
Hana Widyatari <hanawidyatari23@gmail.com>
2022-08-30 13:20:30 +0700

    add margin x header

--- components/headers/PageHeader.js
+++ components/headers/PageHeader.js
@@ -3,14 +3,14 @@ import CreateButton from "../buttons/CreateButton";
 export default function PageHeader(props) {
   return (
     <div>
-      <div className="flex justify-between mx-8">
+      <div className="flex justify-between mx-20">
         <div className="flex gap-8">
           <div className="text-4xl font-semibold">{props.title}</div>
           <div className="mt-1">
             <CreateButton titleButton={props.titleButton} />
           </div>
         </div>
-        <div className="mt-2">
+        <div className="mt-2 cursor-pointer">
           <h3 className="text-blue-500 font-semibold">Logout</h3>
         </div>
       </div>
