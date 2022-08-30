f3900aa7f80ecd90731a63c926817bc1474f1787
Hana Widyatari <hanawidyatari23@gmail.com>
2022-08-30 13:04:19 +0700

    add logim and register page

---
+++ pages/register/index.js
@@ -0,0 +1,19 @@
+import RegisterForm from "../../components/forms/RegisterForm";
+
+export default function index() {
+  return (
+    <div className="flex flex-col justify-center">
+      <div className="mx-auto mt-20">
+        <div>
+          <RegisterForm />
+        </div>
+        <div className="flex ml-[150px] -mt-8 text-sm">
+          <p className="text-gray-800 ">Already have account?</p>
+          <a href="/login" className="text-blue-600 underline ml-3">
+            <p>Login</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
