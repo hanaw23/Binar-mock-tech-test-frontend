f4edee1bf6372642cf4c794b00b69f5a7ff4820b
Hana Widyatari <hanawidyatari23@gmail.com>
2022-08-30 15:07:35 +0700

    add ui edit product

--- components/cards/ProductCard.js
+++ components/cards/ProductCard.js
@@ -7,7 +7,7 @@ export default function ProductCard(props) {
     <div>
       <div className="h-fit w-fit border border-gray-800 bg-slate-800">
         <div className="h-[300px] w-[350px] flex justify-end gap-5 pt-4 pr-5">
-          <EditButton />
+          <EditButton name={props.name} price={props.price} id={props.id} image={props.image} />
           <DeleteButton name={props.name} id={props.id} />
         </div>
         <div className="bg-white h-[100px]">
--- components/modal/EditModal.js
+++ components/modal/EditModal.js
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom";
 
-import CreateProductForm from "../forms/CreateProductForm";
 import style from "./Modal.module.css";
+import EditProductForm from "../forms/EditProductForm";
 
-export default function CreateModal(props) {
+export default function EditModal(props) {
   return ReactDOM.createPortal(
     <>
       <div className={`${style.animated} ${style.faster} ${style.fadeIn} main-modal fixed w-full h-100 inset-0 z-50 overflow-auto flex  justify-center items-center`} style={{ background: "rgba(0,0,0,.7)" }}>
@@ -11,10 +11,10 @@ export default function CreateModal(props) {
           <div>
             <div className="w-full h-50 bg-white px-6 py-6 border-2 border-gray-900">
               <div>
-                <h1 className="text-2xl font-semibold">Create new</h1>
+                <h1 className="text-2xl font-semibold">Edit Product</h1>
               </div>
               <div className="text-center">
-                <CreateProductForm onClose={props.onClose} />
+                <EditProductForm onClose={props.onClose} name={props.name} id={props.id} image={props.image} price={props.price} />
               </div>
             </div>
           </div>
