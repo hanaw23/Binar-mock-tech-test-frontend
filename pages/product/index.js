8c755400cefa9dda813c311f21bcfdc6b1fa877f
Hana Widyatari <hanawidyatari23@gmail.com>
2022-08-31 23:15:42 +0700

    add pagination

--- pages/product/index.js
+++ pages/product/index.js
@@ -8,11 +8,14 @@ import { protectedPage } from "../../utility/protectedPage";
 
 import PageHeader from "../../components/headers/PageHeader";
 import ProductCard from "../../components/cards/ProductCard";
+import Pagination from "../../components/pagination/Pagination";
 import Spinner from "../../components/loadings/Spinner";
 
 export default function index() {
   const [productList, setProductList] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(9);
 
   const dispatch = useDispatch();
 
@@ -24,6 +27,14 @@ export default function index() {
     dispatch(fetchGetProducts(setProductList, setLoading));
   }, []);
 
+  const indexOfLastProducts = currentPage * productsPerPage;
+  const indexOfFirstProducts = indexOfLastProducts - productsPerPage;
+  const currentProducts = productList.slice(indexOfFirstProducts, indexOfLastProducts);
+
+  const handleChangePage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className="w-screen">
       <div className="border border-b-gray-800 py-8 w-screen">
@@ -31,7 +42,7 @@ export default function index() {
       </div>
       {loading ? (
         <div className="mt-10 grid grid-cols-3 w-fit h-fit  ml-8 mx-10 desktop:ml-[150px] gap-y-10 px-4 py-4">
-          {productList.map((item, i) => {
+          {currentProducts.map((item, i) => {
             return (
               <div key={i}>
                 <ProductCard name={item.name} price={item.price} id={item.id} image={item.imageurl} />
@@ -42,6 +53,7 @@ export default function index() {
       ) : (
         <Spinner />
       )}
+      <Pagination productsPerPage={productsPerPage} totalProducts={productList.length} paginate={handleChangePage} />
     </div>
   );
 }
